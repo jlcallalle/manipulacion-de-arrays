60490b38157d49dd36fc24065b35e078bc21bded
Jorge Callalle <jorge.callalle@vasslatam.com>
2023-07-19 02:13:01 -0500

    ejercicio duplicar valores arrays

--- server/02-map.js
+++ server/02-map.js
@@ -13,3 +13,26 @@ for (let index = 0; index < letters.length; index++) {
 
 console.log('original', letters);
 console.log('new', newArray);
+
+// Ejercicio multiplicar x 2 array
+
+const numbers = [2, 4, 5, 6, 8];
+
+/* const doubles = numbers.map(function(item) {
+    return item * 2; // return item = item * 2;
+}); */
+
+const doubles = numbers.map(item => item * 2);
+console.log('numbers', numbers);
+console.log('doubles', doubles);
+
+// con function 
+function solution(array) {
+	return array.map(item => item * 2);
+}; 
+
+const array = [2,4,5,6];
+const solutionArray = solution(array);
+
+console.log(array)
+console.log(solutionArray)
