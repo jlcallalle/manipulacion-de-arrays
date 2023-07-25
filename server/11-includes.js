b647db1ed36f36f89495a2cbe30091f5dec716e5
Jorge Callalle <jorge.callalle@vasslatam.com>
2023-07-25 18:01:45 -0500

    ejercicio include

--- server/11-includes.js
+++ server/11-includes.js
@@ -12,3 +12,25 @@ for (let index = 0; index < pets.length; index++) {
 const rta = pets.includes('dog')
 console.log('for', includeInArray);
 console.log('includes', rta);
+
+// Ejercicio búsqueda 
+/* Input:
+array: ["ana", "santi", "nico", "anastasia"]
+term: "ana"
+*/
+
+const words = ["ana", "santi", "nico", "anastasia"];
+const query = "an";
+
+function filterByTerm(words, query) {
+	const result = words.filter(word => {
+		return word.includes(query);
+	});
+	return result;
+};
+
+const resultado = filterByTerm(words, query);
+console.log('original', words);
+console.log('resultado', resultado);
+
+// filterByTerm(["ana", "santi", "nico", "anastasia"], "an");
